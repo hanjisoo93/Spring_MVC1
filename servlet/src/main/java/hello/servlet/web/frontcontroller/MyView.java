c9362a33c56f3b41f64bd3534d346ddb2b7b3b33
JisooHan <hanjisoo93@gmail.com>
2023-10-13 15:56:14 +0900

    섹션 4. MVC 프레임워크 만들기 : Model 추가 -v3 진행중
    [Note]
    오류 개선도 필요

---
+++ servlet/src/main/java/hello/servlet/web/frontcontroller/MyView.java
@@ -0,0 +1,26 @@
+package hello.servlet.web.frontcontroller;
+
+import jakarta.servlet.RequestDispatcher;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+import java.util.Map;
+import java.util.Objects;
+
+public class MyView {
+    private String viewPath;
+
+    public MyView(String viewPath) {
+        this.viewPath = viewPath;
+    }
+
+    public void render(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException{
+        RequestDispatcher dispatcher = request.getRequestDispatcher(viewPath);
+        dispatcher.forward(request, response);
+    }
+
+    public void render(Map<String, Objects> model, HttpServletRequest request, HttpServletResponse response) {
+    }
+}
